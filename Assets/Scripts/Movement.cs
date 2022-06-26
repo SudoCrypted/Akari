60d41a02ba4e92d6a4529f5e25341efba3245164
SudoCrypted <74665531+SudoCrypted@users.noreply.github.com>
2022-06-26 15:44:49 +0200

    Script Added.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    [SerializeField]
+    float Speed;
     // Start is called before the first frame update
     void Start()
     {
