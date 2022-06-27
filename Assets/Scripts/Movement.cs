8e172ea7046dd4a42784942382f6eafbfb2fa623
SudoCrypted <74665531+SudoCrypted@users.noreply.github.com>
2022-06-27 16:24:30 +0200

    Movement Added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    new Rigidbody2D rigidbody;
+    public float speed = 1000.0f;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.isKinematic = false;
+        rigidbody.angularDrag = 0.0f;
+        rigidbody.gravityScale = 0.0f;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    void Move(Vector2 targetVelocity)
+    {
+        rigidbody.velocity = (targetVelocity * speed) * Time.deltaTime;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 AxisControl = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Move(AxisControl);
     }
 }
