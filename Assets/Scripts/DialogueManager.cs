039e2ac40193bfdb09b78eff11874465f9ea548e
SudoCrypted <74665531+SudoCrypted@users.noreply.github.com>
2022-06-28 13:32:30 +0200

    Text Anchored.

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     Dialogue dialogue;
     RectTransform recttransform;
+    TextMeshPro textmeshpro;
     ProximityDetector proximitydetector;
     [SerializeField] GameObject NPC;
 
@@ -14,20 +16,25 @@ public class DialogueManager : MonoBehaviour
     {
         dialogue = NPC.GetComponent<Dialogue>();
         proximitydetector = NPC.GetComponent<ProximityDetector>();
+        textmeshpro = GetComponent<TextMeshPro>();
+        recttransform = GetComponent<RectTransform>(); // transformmanagement
+    }
+
+    void FixedUpdate()
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 NPC_Pos = dialogue.transform.position; // transform management
-        Vector2 Text_Pos = NPC_Pos + new Vector2(0.0f,2.0f); // transform management
+        Vector2 Text_Pos = NPC_Pos + new Vector2(0.0f,1.2f); // transform management
 
-        recttransform = GetComponent<RectTransform>(); // transformmanagement
+        recttransform.anchoredPosition = Text_Pos;
 
         if (proximitydetector.inRange())
         {
+            textmeshpro.text = "Hi.";
         }
-
-        Debug.Log(proximitydetector.inRange());
     }
 }
