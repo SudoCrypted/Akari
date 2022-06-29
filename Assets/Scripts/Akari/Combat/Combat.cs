d1553f51a283dbdf8b0649295337f6280d2edcc4
SudoCrypted <74665531+SudoCrypted@users.noreply.github.com>
2022-06-29 22:26:48 +0200

    IT WORKS!
    fix: I had to constantly reset the baseinstance target in the update function of combat to allow it to switch between NPC instances.

--- Assets/Scripts/Akari/Combat/Combat.cs
+++ Assets/Scripts/Akari/Combat/Combat.cs
@@ -17,6 +17,7 @@ public class Combat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        baseinstance = Target.GetComponent<BaseInstance>();
         if (baseinstance.proximitydetector.inRange() && Input.GetButtonDown("Fire1"))
         {
             baseinstance.health.LoseHealth(10.0f);
